eee35e6b66fb4ce4512fa8dfe09c4cabff09b4c4
manish713 <bishtmanish713@gmail.com>
2023-03-14 12:37:46 +0530

    updated

--- sensor/config.py
+++ sensor/config.py
@@ -8,6 +8,11 @@ import os
 @dataclass
 class EnvironmentVariable:
     mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id:str = os.getenv("ASW_SECRET_ACCESS_KEY")
+    aws_secret_access_key:str = os.getenv("ASW_SECRET_ACCESS_KEY")
 
-client = pymongo.MongoClient()
+
+env_var = EnvironmentVariable()
+
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
 
