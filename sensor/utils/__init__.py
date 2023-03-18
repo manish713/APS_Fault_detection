4ec514371005acbac12f64c564b9ed81f5703eae
manish713 <bishtmanish713@gmail.com>
2023-03-18 22:47:49 +0530

    data ingestion completed

--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -5,14 +5,23 @@ from sensor.config import mongo_client
 
 
 def get_collection_as_dataframe(database_name:str, collection_name:str)->pd.DataFrame:
+    """
+    Description: This function returns colleciton as dataframe
+    Params: 
+
+    data_name: database name
+    collection_name: collection name
+    ========================================
+    returns Pandas dataframe of a collection      
+    """
     try:
-        logging.info(f"Reading data from database: {database_name} and collection {collection_name}")
+        logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
         df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
         logging.info(f"Found columns: {df.columns}")
         if "_id" in df.columns:
             logging.info(f"Dropping column: _id")
             df = df.drop("_id", axis = 1)
-        logging.info("Row and columns in df: {df.shape}")
+        logging.info(f"Row and columns in df: {df.shape}")
         return df
     except Exception as e:
         raise SensorException(e, sys)
